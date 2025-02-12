1185c16eb8f4395c833268d410448c1be41235d7
ThibautJoncour <96741150+ThibautJoncour@users.noreply.github.com>
2025-02-12 17:05:10 +0100

    Add files via upload

--- Pages/Floor.razor.cs
+++ Pages/Floor.razor.cs
@@ -1,9 +1,9 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MagicNumber
+namespace MagicNumber.Pages
 {
-    public class IndexModel : PageModel
+    public class FloorModel : PageModel
     {
         public void OnGet()
         {
--- Pages/Index.cshtml.cs
+++ Pages/Index.cshtml.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
-namespace MagicNumber
+namespace MagicNumber.Pages
 {
     public class IndexModel : PageModel
     {
